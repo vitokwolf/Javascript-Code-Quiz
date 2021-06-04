5807dd0130f0983b5b520fa38af5767ba0073a8c
Victor Lupascu <sinajeen@gmail.com>
2021-06-04 14:37:00 -0500

    add more questions, adjusted the clear highscores to render on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
 // Questions and Answers Array
 var questions = [
     {
+        question: 'What would following code return? console.log(typeof typeof 1);',
+        choiceA: '1. string',
+        choiceB: '2. number',
+        choiceC: '3. Syntax error',
+        choiceD: '4. undefined',
+        correct: 'A'
+    },
+    {
         question: 'Commonly used data types DO NOT include:',
         choiceA: '1. strings',
         choiceB: '2. booleans',
         choiceC: '3. alerts',
         choiceD: '4. numbers',
         correct: 'C'
+    },
+    {
+        question: 'Which software company developed JavaScript?',
+        choiceA: '1. Mozilla',
+        choiceB: '2. Netscape',
+        choiceC: '3. Sun Microsystems',
+        choiceD: '4. Oracle',
+        correct: 'B'
     },
     {
         question: 'The condition in an if/else statement is enclosed with _____.',
@@ -23,6 +39,14 @@ var questions = [
         choiceC: '3. booleans',
         choiceD: '4. all of the above',
         correct: 'D'
+    },
+    {
+        question: 'What will the code below output to the console? console.log(1 + +"2" + "2");',
+        choiceA: '1. "32"',
+        choiceB: '2. "122"',
+        choiceC: '3. "13"',
+        choiceD: '4. "14"',
+        correct: 'A'
     },
     {
         question: 'String values must be enclosed with _____ when being assigned to variables.',
@@ -227,5 +251,6 @@ function goBack() {
 // Clear High Scores Button
 document.getElementById('clear').addEventListener('click', function () {
     localStorage.clear();
-    alert('Scores have been cleared. Please refresh your page.')
+    alert('Scores have been cleared. Please refresh your page.');
+    highScores();
 });
